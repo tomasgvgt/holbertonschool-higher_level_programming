d7c5d051c0cac9246c4d21ac1864ead1cf976e14
tomasgvgt <tomasgvg.t@gmail.com>
2020-09-01 00:22:50 +0000

    100

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 import sys
-text = "and that piece of art is useful - Dora Korpan, 2015-10-19"
+text = "and that piece of art is useful - Dora Korpar, 2015-10-19"
 sys.stderr.write(str(text) + '\n')
 exit(1)
