97320effc12fa2dfd65694a87179e0c049a37100
tomasgvgt <tomasgvg.t@gmail.com>
2020-09-11 00:44:42 +0000

    7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def update_dictionary(a_dictionary, key, value):
+    for i in a_dictionary:
+        if i == key:
+            a_dictionary[i] = value
+            return a_dictionary
+    a_dictionary[key] = value
+    return a_dictionary
