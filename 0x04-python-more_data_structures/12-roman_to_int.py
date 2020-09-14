e44c748562ee5690887f6868b4606b218b9addf7
tomasgvgt <tomasgvg.t@gmail.com>
2020-09-14 19:03:04 +0000

    12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@ def roman_to_int(roman_string):
     r_sym = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
 
-    if not roman_string:
+    if not roman_string or type(roman_string) is not str:
         return None
     for i in range(len(roman_string)):
         if i < len(roman_string) - 1:
