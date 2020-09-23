4e44456f60303969b746b3bc8471aceb9fc2a14e
tomasgvgt <tomasgvg.t@gmail.com>
2020-09-23 00:14:49 +0000

    6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -31,7 +31,7 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if (isinstance(value, tuple) and (len(vale) == 2) and
+        if (isinstance(value, tuple) and (len(value) == 2) and
             (isinstance(value[0], int) and isinstance(value[1], int)) and
                 (value[0] >= 0 and value[1] >= 0)):
             self.__position = value
