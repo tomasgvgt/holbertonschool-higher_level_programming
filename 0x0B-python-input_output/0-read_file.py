bb92a4eaa6c5ff9c8cb9b1ba790ded3b8d38bcf0
tomasgvgt <tomasgvg.t@gmail.com>
2020-10-07 23:29:03 +0000

    0 checker

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,5 +8,5 @@ def read_file(filename=""):
     """function that reads a text file (UTF8)
     and prints it to stdout
     """
-    with open(filename, encoding="UTF*") as f:
+    with open(filename, encoding="UTF8") as f:
         print(f.read())
