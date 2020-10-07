4469d462749778b1ccfd67296502df25e92a717f
tomasgvgt <tomasgvg.t@gmail.com>
2020-10-07 23:36:30 +0000

    2 checker

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -10,5 +10,9 @@ def read_lines(filename="", nb_lines=0):
     """
 
     with open(filename, "r", encoding="utf-8") as f:
-        for line in f:
-            print(line, end='')
+        if nb_lines > 0:
+            for i in range(nb_lines):
+                print(f.readline(), end="")
+        else:
+            for line in f:
+                print(line, end='')
