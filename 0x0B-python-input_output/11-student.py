19ec3807ba42b9d8a4754d0c362fe0fd91d787bf
tomasgvgt <tomasgvg.t@gmail.com>
2020-10-08 02:56:14 +0000

    11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,17 @@
+#!/usr/python3
+"""COntains:
+class Student
+"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """Initializes Student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation of
+        a Student instance"""
+        return self.__dict__
