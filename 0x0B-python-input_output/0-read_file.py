3992c02e600ad2c6b9804cbe73716c1e2f91af9d
tomasgvgt <tomasgvg.t@gmail.com>
2020-10-08 13:50:40 +0000

    0 in a cleaner way

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,5 +9,4 @@ def read_file(filename=""):
     and prints it to stdout
     """
     with open(filename, encoding="utf-8") as f:
-        for line in f:
-            print(line, end="")
+        print(f.read(), end="")
