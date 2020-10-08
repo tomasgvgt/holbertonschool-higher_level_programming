091e6d2e0acb72f4e397b740bcfc1a1ec686ceb8
tomasgvgt <tomasgvg.t@gmail.com>
2020-10-08 14:41:49 +0000

    0 more documentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,5 +8,7 @@ def read_file(filename=""):
     """function that reads a text file (UTF8)
     and prints it to stdout
     """
+    # the with keyword, closes the file when the block inside
+    # is finnished.
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
