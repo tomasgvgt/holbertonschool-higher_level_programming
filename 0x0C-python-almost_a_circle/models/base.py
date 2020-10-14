53464ebd474bf4cdfb67de9da254bbe43fdcc633
tomasgvgt <tomasgvg.t@gmail.com>
2020-10-14 01:52:01 +0000

    to_json_string update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,8 +21,8 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """ returns the JSON string representation of list_dictionaries"""
-        if list_dictionaries is None or list_dictionaries == []:
-            return []
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
         return json.dumps(list_dictionaries)
 
     @staticmethod
