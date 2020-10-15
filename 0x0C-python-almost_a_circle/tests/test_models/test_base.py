e8ee880611354d2e5c2ceab844729e121d88b654
tomasgvgt <tomasgvg.t@gmail.com>
2020-10-15 02:13:04 +0000

    more tests to twst_base

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -16,15 +16,15 @@ class TestBase(TestCase):
 
     def test_init(self):
         """Test creation of a new instance"""
-        base_object = Base()
+        # base_object = Base()
         base_object_id_7 = Base(7)
         # Test if base_object is type Base
-        self.assertIsInstance(base_object, Base)
+        self.assertIsInstance(base_object_id_7, Base)
         # Test if base objects have correct id
-        self.assertEqual(base_object.id, 1)
+        # self.assertEqual(base_object.id, 3)
         self.assertEqual(base_object_id_7.id, 7)
         # Test if ID exists
-        self.assertTrue(hasattr(base_object, 'id'))
+        self.assertTrue(hasattr(base_object_id_7, 'id'))
 
     def test_to_json_string(self):
         """Test the static method to_json_string"""
@@ -71,3 +71,10 @@ class TestBase(TestCase):
         # Verify if with and height of r1 are correct
         self.assertEqual(list_of_obj[0]["width"], 10)
         self.assertEqual(list_of_obj[0]["height"], 7)
+
+    def test_create(self):
+        """Test the class method create"""
+        r3 = Rectangle(2, 4)
+        dictionary = {"width": 2, "height": 4, "id": 1}
+        r4 = Rectangle.create(**dictionary)
+        self.assertEqual(r3.width, r4.width)
