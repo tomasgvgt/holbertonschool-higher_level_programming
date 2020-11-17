4e1119d5c08131fbaefa734555fc724af68a1be0
tomasgv1 <tomasgv.biz@gmail.com>
2020-11-16 21:01:25 -0500

    13-14

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,6 @@
+-- Query that computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- The result column name should be average.
+SELECT
+	AVG(score) "average"
+FROM
+	second_table;
