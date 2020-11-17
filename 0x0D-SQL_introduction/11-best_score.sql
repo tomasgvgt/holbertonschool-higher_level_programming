1640d02c0e967fb250ed86f128602ace40dc0d37
tomasgv1 <tomasgv.biz@gmail.com>
2020-11-16 20:23:25 -0500

    11

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,12 @@
+-- query that lists all records with a score >= 10 of the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- Results should display both the score and the name (in this order).
+-- Records should be ordered by score (top first).
+SELECT
+	score,
+	name
+FROM
+	second_table
+WHERE
+	score >= 10
+ORDER BY
+	score DESC;
