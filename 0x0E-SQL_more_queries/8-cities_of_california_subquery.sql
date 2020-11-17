2e5ca6ff030726a00dbad801afd670032fb0fe2b
tomasgv1 <tomasgv.biz@gmail.com>
2020-11-17 15:58:25 -0500

    8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -3,5 +3,5 @@
 SELECT id, name
 FROM hbtn_0d_usa.cities
 WHERE state_id = (
-	SELECT id FROM hbtn_0d_usa.states WHERE name = 'California')
+	SELECT id FROM states WHERE name = 'California')
 ORDER BY cities.id ASC;
