ae135358adb1910dfc1f09ae82a89561ec49f7bd
tomasgv1 <tomasgv.biz@gmail.com>
2020-11-17 15:43:56 -0500

    7 correction 3

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -7,6 +7,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
 	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 	state_id INT NOT NULL,
-	FOREIGN KEY(state_id) REFERENCES states(id),
+	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
 	name VARCHAR(256) NOT NULL
 );
