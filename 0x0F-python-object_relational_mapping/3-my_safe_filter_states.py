ff288f47e0394765d5938f041b94f6d8c9a01465
tomasgvgt <tomasgvg.t@gmail.com>
2020-12-13 20:44:17 -0500

    change to 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
         host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     c = database.cursor()
     c.execute(
-        "SELECT id, name "
+        "SELECT *"
         "FROM states WHERE name = '%s' ORDER BY states.id ASC" % argv[4]
     )
     query_rows = c.fetchall()
