14313195fc7e9ba21a5864f901269dcf34eec8a4
tomasgvgt <tomasgvg.t@gmail.com>
2020-12-18 16:52:05 -0500

    takes in a URL and displays all HTTP methods the server will accept.

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, and displays all HTTP methods the server will accept.
-curl -X OPTIONS -s "$1"
+curl --head "$1" | grep -e 'Allow' | cut -d ":" -f 2 | cut -d " " -f 2-
