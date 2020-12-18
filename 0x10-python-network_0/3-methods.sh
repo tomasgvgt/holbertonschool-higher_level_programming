761fffe1d3fc95e747e6bca0e68d3fba9a4adc3a
tomasgvgt <tomasgvg.t@gmail.com>
2020-12-18 16:57:03 -0500

    cheker issues

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, and displays all HTTP methods the server will accept.
-curl --head "$1" | grep -e 'Allow' | cut -d " " -f 2-
+curl --head "$1" | grep 'Allow' | cut -d " " -f 2-
