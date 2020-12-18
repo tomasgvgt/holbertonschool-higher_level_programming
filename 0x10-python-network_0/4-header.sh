9f5fb35013d5b1aeb26e61ef08410147e170fb57
tomasgvgt <tomasgvg.t@gmail.com>
2020-12-18 17:18:41 -0500

    script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # script sends a GET request to the URL, and displays the body of the response., # A header variable X-HolbertonSchool-User-Id must be sent with the value 98
-
 curl "$1" -H "X-HolbertonSchool-User-Id: 98"
