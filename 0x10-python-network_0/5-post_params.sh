cd4e38f9c3b83a2697795740cba87004210fa28e
tomasgvgt <60365542+tomasgvgt@users.noreply.github.com>
2021-03-22 13:37:55 -0500

    Delete 0x10-python-network_0 directory

--- 0x10-python-network_0/5-post_params.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# script sends a GET request to the URL, and displays the body of the response., multiple variables must be set
-curl "$1" -s -d "email=hr@holbertonschool.com" -d "subject=I will always be here for PLD"
