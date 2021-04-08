c1a0e0d641356db209c7914140cd9967e2da35eb
tomasgvgt <tomasgvg.t@gmail.com>
2021-04-08 15:41:16 -0500

    Checker doesnt like me

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,5 +2,4 @@
 # Bash script that takes in a URL, sends a request to that URL (Using curl)
 # and displays the size of the body of the response
 # The size must be displayed in bytes
-
 curl "$1" -s | wc -c
