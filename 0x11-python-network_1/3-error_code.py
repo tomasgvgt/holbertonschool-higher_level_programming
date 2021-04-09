3ddb40a8b7c58262dcff1aeeb62a08e4c7c0c6a5
tomasgvgt <tomasgvg.t@gmail.com>
2021-04-09 16:42:44 -0500

    3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,7 +5,7 @@ displays the value of the X-Request-Id variable found
 in the header of the response.
 """
 import sys
-from urllib import request
+from urllib import request, error
 
 
 if __name__ == "__main__":
