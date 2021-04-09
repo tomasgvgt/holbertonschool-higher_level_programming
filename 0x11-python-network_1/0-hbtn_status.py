2ae648eab2964259e22fd13e446d6563672dde5f
tomasgvgt <tomasgvg.t@gmail.com>
2021-04-09 14:59:21 -0500

    Checker

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,10 +3,13 @@
 Python script that fetches https://intranet.hbtn.io/status
 """
 from urllib import request
-with request.urlopen("https://intranet.hbtn.io/status") as resp:
-    data = resp.read()
-html = data.decode("UTF-8")
-obj_type = type(data)
-print("\t- type: {}".format(obj_type))
-print("\t- content: {}".format(data))
-print("\t- utf8 content: {}".format(html))
+
+if __name__== "__main__": 
+    with request.urlopen("https://intranet.hbtn.io/status") as resp:
+        data = resp.read()
+    html = data.decode("UTF-8")
+    obj_type = type(data)
+    print("Body response:")
+    print("\t- type: {}".format(obj_type))
+    print("\t- content: {}".format(data))
+    print("\t- utf8 content: {}".format(html))
