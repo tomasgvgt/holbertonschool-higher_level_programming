b332810e0e8b0591def907bf93fe21341748b6cc
tomasgvgt <tomasgvg.t@gmail.com>
2021-04-09 14:54:39 -0500

    0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Python script that fetches https://intranet.hbtn.io/status
+"""
+from urllib import request
+with request.urlopen("https://intranet.hbtn.io/status") as resp:
+    data = resp.read()
+html = data.decode("UTF-8")
+obj_type = type(data)
+print("\t- type: {}".format(obj_type))
+print("\t- content: {}".format(data))
+print("\t- utf8 content: {}".format(html))
