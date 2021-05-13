f5876402c761e0ab409fa6e633b0889174e6ec47
tomasgvgt <tomasgvg.t@gmail.com>
2021-05-12 21:09:30 -0500

    6

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,10 @@
+/*
+JavaScript script that updates the text of the <header> element to New Header!!! when the user clicks on DIV#update_header
+
+    You can’t use document.querySelector to select the HTML tag
+    You must use the JQuery API
+    html file: 6-main.html
+*/
+$('#update_header').click(() => {
+  $('header').text('New Header!!!');
+});
