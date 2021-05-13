a7da5eef92f6b43cb541b22cdfad6eb67460ec1b
tomasgvgt <tomasgvg.t@gmail.com>
2021-05-12 20:24:37 -0500

    4

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,12 @@
+/*
+JavaScript script that toggles the class of the <header> element when the user clicks on the tag DIV#toggle_header:
+
+    The <header> element must always have one class: red or green, never both in the same time and never empty.
+    If the current class is red, when the user click on DIV#toggle_header, the class must be updated to green ; and the reverse.
+    You can’t use document.querySelector to select the HTML tag
+    You must use the JQuery API
+    html file: 4-main.html
+*/
+$('#toggle_header').click(() => {
+  $('header').toggleClass('green red');
+});
